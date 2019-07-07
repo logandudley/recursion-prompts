0164ef50e232229fc5a7ddfdc778ef624faee848
Logan <logandudley@gmail.com>
2019-07-07 13:47:12 -0700

    Implement range

--- src/recursion.js
+++ src/recursion.js
@@ -63,6 +63,12 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+	if(Math.abs(y - x) < 2) return [];
+	let dif = x < y ? 1 : -1;
+	if(Math.abs(y - x) === 2) return x + dif;
+	else {
+		return [x+dif].concat(range(x+dif,y));
+	}
 };
 
 // 7. Compute the exponent of a number.
